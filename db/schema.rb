e100f88e7e9c8b2d03c6f8a9fa67a4c7b30a9abd
mateusc42 <mcgs@cin.ufpe.br>
2018-03-08 22:51:00 -0300

    Criação do CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308213340) do
+ActiveRecord::Schema.define(version: 20180309012110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.integer "role"
@@ -34,4 +43,5 @@ ActiveRecord::Schema.define(version: 20180308213340) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
