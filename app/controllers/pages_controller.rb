5e47ac0ebdd698cbc2e06ebf62613188fbcfda71
mateusc42 <mcgs@cin.ufpe.br>
2018-03-15 14:08:05 -0300

    Management Panel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,20 @@
 class PagesController < ApplicationController
+	before_action :superadmin_only, :except => :about
 	
 	def about
 	end
+
+	def dashboard
+		@posts = Post.all
+		@users = User.all
+	end
+
+	private
+
+  def superadmin_only
+    unless current_user.superadmin?
+      redirect_to root_path, :alert => "Access denied."
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 	root "posts#index"
 	get '/myposts', to: 'posts#myposts'
 	get '/about', to: 'pages#about'
+	get '/dashboard', to: 'pages#dashboard'
 	
 	resources :comments do
     resources :comments
@@ -11,5 +12,5 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-	devise_for :users, :controllers => { :registrations => 'registrations' }
+  devise_for :users, :controllers => { :registrations => 'registrations' }
 end
